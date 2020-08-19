2314b94d716af52b27226d0c06ff59cb18ce6be5
VineethChandha <69765021+VineethChandha@users.noreply.github.com>
2020-08-19 23:05:40 +0530

    Basic if else ladder

---
+++ use_of_IF.py
@@ -0,0 +1,23 @@
+# Program explaining basic if else ladder
+
+num1 = 200
+num2 = 100
+if num1>num2:
+    print("num1>num2")
+elif num2>num1:
+    print("num2>num1")
+else:
+    print ("both are equal")
+if not num1>=num2:
+    print("num2>num1")
+elif not num2>=num1:
+    print("num1>num2")
+else:
+    print ("both are equal")
+
+#True
+#False (booleans cannot be all in lowercase and cannot be inside " ")
+#and operator (both are true it is true otherwise false)
+#or operator(both are false it is false otherwise true)
+#should use the operators in lowercase
+#Python list can have multiple data types. List = [1,2,"a",True]   
