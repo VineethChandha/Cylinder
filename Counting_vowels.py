5024467f053413fdf5888f6da78bdf5d6500d718
VineethChandha <69765021+VineethChandha@users.noreply.github.com>
2020-08-25 00:08:26 +0530

    Counting Vowels & Consonants in the given word

---
+++ Counting_vowels.py
@@ -0,0 +1,17 @@
+#This code helps us to find number of vowels and consonents in the given word
+
+vowels = 0
+consonents = 0
+word = input("Tell the word: ").lower().strip()
+for letter in word:
+    if letter in "aeiou":
+   # if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u" :
+        vowels=vowels+1
+    elif letter==" ":
+        pass
+    else:
+        consonents=consonents+1
+print("Number of vowels in the word are {}.".format(vowels))
+print("Number of consonents in the word are {}.".format(consonents))
+
+        
