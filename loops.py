1ae2e38433d4b0f1ccfdd6cd4273072c8f197772
VineethChandha <69765021+VineethChandha@users.noreply.github.com>
2020-08-25 00:34:56 +0530

    Update loops.py

--- loops.py
+++ loops.py
@@ -4,9 +4,6 @@ for a in range(1,11):   # a will be valuated from 1 to 10
     print("Hi")
     print(a)
 
-    
-# List Comprehensions
-
 even_numbers = [x for x in range(1,100) if x%2 == 0]
 print(even_numbers)
 odd_numbers = [x for x in range(1,100) if x%2 != 0]
@@ -18,6 +15,10 @@ print(answer)
 word = ["Happy","Birthday","To","You"]
 word="-".join(word)     #Join key word is used to join the list of the words with some specific sign or space as required
 print(word)
+
+Word2 = "Happy Birthday To You"
+split_word = Word2.split()      #.split() funtion is used to make the sentance to the list of words
+print(split_word)
 
 
 # This brings out the names of friends in which letter a is present
