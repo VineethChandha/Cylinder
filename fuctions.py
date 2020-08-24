34f2d0fb8393b58214e3786bc0a27ae36afc4d02
VineethChandha <69765021+VineethChandha@users.noreply.github.com>
2020-08-25 00:42:42 +0530

    Introduction to functions in python

---
+++ fuctions.py
@@ -0,0 +1,15 @@
+#This is a program to introduce the Finctions concept and using it created a string reverse program
+
+text=input("enter the name to get the reverse name: ").strip().lower()
+def rev(x):
+    return x[::-1]
+reverse = rev(text)
+print(reverse)
+
+def about(name,age,like="football"):
+    #foot ball will be defalut value if the fuctioncal does not contain like
+    #defalut parameters should be at last
+    s="{} is {} years old and likes {}".format(name,age,like)
+    print(s)
+about("ram",23,"tennis")
+about("ram",23)  # ram,23,tennies are called arguments
